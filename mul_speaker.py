2440e81712f8af53be1ea9a32e4fbeb9ef339844
Nirmal Shaju <alduinnirmal@gmail.com>
2021-06-07 23:18:01 +0530

    second commit

--- mul_speaker.py
+++ mul_speaker.py
@@ -1,17 +1,25 @@
-num = input("Enter a number")
+import pyttsx3
+num = input("Enter a number\n")
 #declare a dictonary for store number equivalent text-values
 
 dic = {
-    "1" : "ones"
-    "2" : "twos"
-    "3" : "threes"
-    "4" : "fours"
-    "5" : "fives"
-    "6" : "sixes"
-    "7" : "sevens"
-    "8" : "eights"
-    "9" : "nines"
+    "1" : "ones",
+    "2" : "twos",
+    "3" : "threes",
+    "4" : "fours",
+    "5" : "fives",
+    "6" : "sixes",
+    "7" : "sevens",
+    "8" : "eights",
+    "9" : "nines",
     "10" : "tens"
 }
 
 txt = ""  #
+
+for i in range(1,11):
+    mul = i* int(num)
+    txt += str(i)+ " " + dic[num] +" are " +str(mul) +"\n"
+engine = pyttsx3.init()
+engine.say(txt)
+engine.runAndWait()
