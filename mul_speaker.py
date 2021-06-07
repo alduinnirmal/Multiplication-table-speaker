d0caa523b2e9ea9757ff8d83ebd2ba7f4aacebea
Nirmal Shaju <alduinnirmal@gmail.com>
2021-06-07 22:37:58 +0530

    first commit

---
+++ mul_speaker.py
@@ -0,0 +1,17 @@
+num = input("Enter a number")
+#declare a dictonary for store number equivalent text-values
+
+dic = {
+    "1" : "ones"
+    "2" : "twos"
+    "3" : "threes"
+    "4" : "fours"
+    "5" : "fives"
+    "6" : "sixes"
+    "7" : "sevens"
+    "8" : "eights"
+    "9" : "nines"
+    "10" : "tens"
+}
+
+txt = ""  #
